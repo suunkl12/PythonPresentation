600eddd08c92a019be753aefcd1447484b0503a9
suunkl12 <40013740+suunkl12@users.noreply.github.com>
2019-12-15 22:02:33 +0700

    Add files via upload

---
+++ SnakeGame/SnakeGame.py
@@ -0,0 +1,367 @@
+import ctypes
+
+kernel32 = ctypes.WinDLL('kernel32')
+user32 = ctypes.WinDLL('user32')
+
+SW_HIDE = 0
+
+hWnd = kernel32.GetConsoleWindow()
+if hWnd:
+    user32.ShowWindow(hWnd, SW_HIDE)
+import math
+import random
+import sys
+import pygame
+
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    rows = 20
+    w = 500
+    def __init__(self,start,dirnx=1,dirny=0, color= (255,0,0)):
+        self.pos = start
+        self.dirnx = 1
+        self.dirny = 0
+        self.color = color
+
+        
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+    
+    
+    def draw(self,surface,eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+
+        pygame.draw.rect(surface,self.color,(i*dis+1,j*dis+1,dis-2,dis-2))
+        if eyes:
+            centre = dis//2
+            radius  = 3
+            circleMiddle = (i*dis+centre-radius,j*dis+8)
+            circleMiddle2 = (i*dis + dis - radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle,radius)
+            pygame.draw.circle(surface,(0,0,0), circleMiddle2, radius)
+        
+class snake(object):
+    body = []
+    turns = {}
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx= 1
+        self.dirny= 0
+    
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+                
+        keys = pygame.key.get_pressed()
+
+        #for key in keys:
+        if keys[pygame.K_LEFT] and self.dirnx != 1:
+                self.dirnx = -1
+                self.dirny = 0
+                self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]                   
+        elif keys[pygame.K_RIGHT] and self.dirnx != -1:
+                self.dirnx = 1
+                self.dirny = 0
+                self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+        elif keys[pygame.K_UP] and self.dirny != 1:
+                self.dirnx = 0
+                self.dirny = -1
+                self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+        elif keys[pygame.K_DOWN]and self.dirny != -1:
+                self.dirnx = 0
+                self.dirny = 1
+                self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+                
+        for i,c in enumerate(self.body):
+            p = c.pos[:] 
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0],turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <=0: c.pos = (c.rows-1,c.pos[1])
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1: c.pos = (0,c.pos[1])
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1: c.pos = ( c.pos[0],0)
+                elif c.dirny == -1 and c.pos[1] <=0 : c.pos = (c.pos[0],c.rows-1)
+                else: c.move(c.dirnx,c.dirny)
+
+                
+    def reset(self,pos):
+        self.head= cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 1
+        self.dirny = 0
+        
+    
+    def addCube(self):
+        tail = self.body[-1]
+        dx,dy = tail.dirnx, tail.dirny
+
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0], tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0], tail.pos[1]+1)))
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+            
+    def draw(self,surface):
+        for i, c in enumerate(self.body):
+            if i ==0:
+                c.draw(surface,True)
+            else:
+                c.draw(surface)
+
+class pointer(object):
+    def __init__(self,start, color= (255,0,0)):
+        self.body = cube(start,color)
+        self.color = color
+        
+    def move(self):
+        global flag,row
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN  and event.key == pygame.K_UP and self.body.pos == (rows/5.8,rows/2.1 +2):
+                self.body.move(0,-2)
+            elif event.type == pygame.KEYDOWN  and event.key == pygame.K_DOWN and self.body.pos == (rows/5.8,rows/2.1 ):
+                self.body.move(0,2)
+            elif event.type == pygame.KEYDOWN  and event.key == pygame.K_RETURN and self.body.pos == (rows/5.8,rows/2.1):
+                flag = False
+            elif event.type == pygame.KEYDOWN  and event.key == pygame.K_RETURN and self.body.pos == (rows/5.8,rows/2.1 + 2):
+                pygame.quit()
+                sys.exit()
+                
+    def draw(self,surface):
+        self.body.draw(surface)
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+    
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (75,75,75), (x,0),(x,w))
+        pygame.draw.line(surface, (75,75,75), (0,y),(w,y))
+
+
+        
+
+def delay():
+    global p
+    p.move()
+    #quitSkipDetect()
+    pygame.time.delay(1)       
+        
+
+def quitDetect():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+def quitSkipDetect():
+    global flag
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN  and event.key == pygame.K_RETURN:
+                flag = False
+                return 
+            
+def displayText(surface,string):
+    global listOfColors
+    listOfColors = [(66, 93, 245),(245, 66, 66),(242, 245, 66),(66, 245, 72),(48, 242, 239)]
+    global color , x,y
+    #while True:
+    #    randColor = random.choice(listOfColors)
+    #    if  randColor == color :
+    #        continue
+    #    else:
+    #        color = randColor
+    #        break
+    quitSkipDetect()
+    surface.fill((0,0,0))
+    x = min (x+25,255)
+    y = min (y+5,49)
+    color = (0,x,y)
+    font = pygame.font.SysFont('Papyrus',width//13)    
+    text = font.render(string,True,color)
+    surface.blit(text,(width//2-text.get_width() // 2,width//2 - text.get_height() // 2))
+    pygame.display.update()
+
+def displayGameTitle(surface,string,start):
+    global width, listOfColors,titleColor
+    listOfColors = [(66, 93, 245),(66, 245, 72),(245, 66, 66)]
+
+    while True:
+        randColor = random.choice(listOfColors)
+        if  randColor == titleColor :
+            continue
+        else:
+            titleColor = randColor
+            break
+    quitDetect()    
+    font = pygame.font.SysFont('papyrus',width//10)
+    text = font.render(string,True,titleColor)
+    pos = (start[0]-text.get_width() // 2,start[1] - text.get_height() // 2)    
+    surface.blit(text,pos)
+
+def displayTextQuick(surface,string,color,start):
+    global width
+    quitDetect()    
+    font = pygame.font.SysFont('arial',width//13)
+    text = font.render(string,True,color)
+    pos = (start[0]-text.get_width() // 2,start[1] - text.get_height() // 2)    
+    surface.blit(text,pos)
+ 
+    
+def fadeText(surface,string):
+    global color , x,y
+    quitSkipDetect()
+    surface.fill((0,0,0))
+    x = max (x-25,0)
+    y = max (y-5,0)
+    color = (0,x,y)
+    font = pygame.font.SysFont('Papyrus',width//13)    
+    text = font.render(string,True,color)
+    surface.blit(text,(width//2-text.get_width() // 2,width//2 - text.get_height() // 2))
+    pygame.display.update()
+
+def redrawWindowStart(surface,string1,string2):
+    global p,width,rows
+    quitSkipDetect()
+    surface.fill((0,0,0))
+    p.draw(surface)
+    titlePos = (width/2,width/3)
+    startPos = (width/2,width/2)
+    quitPos = (width/2.1, width/2.1 + 2.5*width/rows)
+    displayGameTitle(surface,'Snake Game',titlePos)
+    displayTextQuick(surface,string1,(242, 245, 66),startPos)
+    displayTextQuick(surface,string2,(242, 245, 66),quitPos)
+    #displayTextQuick(surface,string,(242, 245, 66))
+    pygame.display.update()
+
+def redrawWindow(surface):
+    global rows,width, s, snack
+    surface.fill((0,0,0))
+    s.draw(surface)
+    snack.draw(surface)
+    drawGrid(width,rows, surface)
+    pygame.display.update() 
+
+def randomSnack( item):
+    positions = item.body
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+            continue
+        else:
+            break
+    return (x,y)
+
+def message_box(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost",True)
+    root.withdraw()
+    messagebox.showinfo(subject,content)
+    try:
+        root.destroy()
+    except:
+        pass
+def main():
+    global width, rows, s,snack
+    global color , x,y, flag,titleColor
+    x=0
+    y=0
+    titleColor = (0,0,0)
+    color = (0,x,y)
+    pygame.init()
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width,width))
+    s = snake((255,0,0),(10,10))
+    snack = cube(randomSnack(s),color= (0,255,0))
+    flag = True
+    clock = pygame.time.Clock()
+
+    title = "Made by Khang"
+    while flag:
+        clock.tick(5)
+        displayText(win,title)                    
+        if ( x== 255 and y == 49):
+            break
+        
+    for i in range(1,2001):
+        if not flag:
+            break
+        quitSkipDetect()
+        pygame.time.delay(1)
+        
+    while flag:
+        clock.tick(5)                    
+        fadeText(win,title)            
+        if ( x== 0 and y == 0):
+            break
+
+    while True:
+        global p
+        p = pointer((rows/5.8,rows/2.1),(245, 66, 66))
+        c = p.body
+        flag = True
+        while flag:
+            clock.tick(10)
+            redrawWindowStart(win,'Press enter to play','Quit')
+            for i in range(1,501):
+                delay()
+            if c.pos == (rows/5.8,rows/2.1):
+                redrawWindowStart(win,'','Quit')
+            else:
+                redrawWindowStart(win,'Press enter to play','')
+            for i in range(1,501):
+                delay()
+            
+        flag = True
+        while flag:
+            pygame.time.delay(50)
+            clock.tick(10)
+            s.move()
+            if s.body[0].pos== snack.pos:
+                s.addCube()
+                snack = cube((randomSnack(s)),color= (0,255,0))
+            caption = ('Snake Game made by Khang - Score: ' + str(len(s.body)-1))
+            pygame.display.set_caption(('Snake Game made by Khang - Score: ' + str(len(s.body)-1)))
+            for x in range(len(s.body)):
+                if s.body[x].pos in list(map(lambda z:z.pos, s.body[x+1:])):
+                    message_box('You Lost!', 'Play again...')
+                    s.reset((10,10))
+                    flag = False
+                    break
+            
+                
+            redrawWindow(win)       
+
+
+
+main()
